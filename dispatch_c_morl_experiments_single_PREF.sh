70c91a34ea927990113f2265fad47a3b0f4fb11f
shaswot <shaswot@gmail.com>
2020-06-27 18:36:54 +0900

    fixed logfilename. changed seq-parallel order. Now all pref run simultaneoulsy but seeds run sequentially

--- dispatch_c_morl_experiments_single_PREF.sh
+++ dispatch_c_morl_experiments_single_PREF.sh
@@ -17,7 +17,7 @@ echo "Seedfile = $seed_filename"
 while IFS= read -r line
 do
     ## reading each line
-    log_filename=$1-g$2-n$3-p$4-$line
+    log_filename=$1sense-g$2-n$3-p$4-$line
     echo "$log_filename"
-    python ./c_morl.py --env="$1" --gamma="$2" --noise="$3" --pref="$4" --seed="$line"  >> logfiles/"$log_filename" 2>&1 &   
+    python ./c_morl.py --env="$1" --gamma="$2" --noise="$3" --pref="$4" --seed="$line"  >> logfiles/"$log_filename" 2>&1   
 done < "$seed_filename"
