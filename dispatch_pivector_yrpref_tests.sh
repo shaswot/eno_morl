14aa6ea62f999334b8e700b290ac0b1e111cb4c0
shaswot_koi <shaswot@gmail.com>
2020-07-05 16:41:14 +0900

    pivector_yrpref batch script added

---
+++ dispatch_pivector_yrpref_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## USAGE
+## Run each experiment with seeds specified seedfile
+# ./dispatch_pivector_yrpref_test.sh <seed_file>
+# ./dispatch_pivector_experiments.sh seedlist.dat
+mkdir -p logfiles
+
+seed_filename=$1
+
+# get file with the list of seeds
+echo "Seedfile = $seed_filename"
+while IFS= read -r line
+do
+    ## reading each line
+    log_filename=$1-pivector_yr_pref-$line
+    echo "$log_filename"
+    python ./running_yrpref_pivector.py --seed="$line"  >> logfiles/"$log_filename" 2>&1 &
+
+done < "$seed_filename"
+
