81b9301c5995c18d2539003841e9ab5fc3de478b
shaswot_koi <shaswot@gmail.com>
2020-07-05 14:07:51 +0900

    koi commit before off-policy

--- dispatch_pivector_experiments.sh
+++ dispatch_pivector_experiments.sh
@@ -16,7 +16,7 @@ echo "Seedfile = $seed_filename"
 while IFS= read -r line
 do
     ## reading each line
-    log_filename=$1-pivector_constant_pref2-g$2-n$3-$line
+    log_filename=$1-pivector_constant_pref8-g$2-n$3-$line
     echo "$log_filename"
 #     python ./pivector.py --env="$1" --gamma="$2" --noise="$3" --seed="$line"  >> logfiles/"$log_filename" 2>&1 &
 #     python ./pivector-random_pref.py --env="$1" --gamma="$2" --noise="$3" --seed="$line"  >> logfiles/"$log_filename" 2>&1 &
